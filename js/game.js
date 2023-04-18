1bf97fc69331c64c102f6989293b2d3d68c3c308
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-04-18 17:08:10 +0000

    added game.js

---
+++ js/game.js
@@ -0,0 +1,32 @@
+/* Global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall all rights reserved.
+//
+// Created by: Mr. Coxall
+// Created on: Oct 2020
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phaser Game
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physcis: {
+    default: 'arcade',
+    arcade: {
+      debug: true
+    },
+  },
+  // set background colour
+  backgroundColor: '#0x5f6e7a,'
+  Scale: {
+    mode: Phaser.Scale.FIT,
+  // we place it in the middle of the page.
+  autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
