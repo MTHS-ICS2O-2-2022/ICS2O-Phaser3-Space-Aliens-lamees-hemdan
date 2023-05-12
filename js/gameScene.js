2433e1d603bc959f28b59e2a64452bcadd46b23f
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-12 16:49:26 +0000

    did phaser3-10

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,6 +1,6 @@
 /* global Phaser */
 
-// Copyright (c) 2020 Mr. Coxall all rights reserved.
+// Copyright (c) 2023 Lamees Hemdan rights reserved.
 //
 // Created by: Lamees Hemdan
 // Created on: April 2023
@@ -18,6 +18,7 @@
 
     this.background = null
     this.ship = null
+    this.fireMissile = false
   }
 
 /**
@@ -39,6 +40,7 @@
 
     this.load.image('starBackground', 'assets/starBackground.png')
     this.load.image('ship', 'assets/spaceShip.png')
+    this.load.image('missile', 'assets/missile.png')
   }
 
   /** 
@@ -51,6 +53,9 @@
     this.background.setOrigin(0, 0)
 
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+
+    // create a group for the missiles
+    this.missiles = this.physics.add.group {}
   }
   
   /**
@@ -59,9 +64,10 @@
    * @param {number} time - The current time
    * @param {number} delta - The delta time in ms since the last frame
     */
-  update(time, delta) {
+    update(time, delta) {
     const keyLeftObj = this.input.keyboard.addKey('LEFT')
     const keyRightObj = this.input.keyboard.addKey('RIGHT')
+    const keySpaceObj = this.input.keyboard.addKey('SPACE')
 
     if (keyLeftObj.isDown === true) {
       this.ship.x -= 15
@@ -74,6 +80,17 @@
       if (this.ship.x > 1920) {
         this.ship.x = 1920
       }
+    }
+    if (keySpaceObj.isDown === true) {
+      if (this.fireMissile = false) {
+        // fire missile
+        this.fireMissile = true
+        const aNewMissile = this.physics.add.sprite(this.ship.x, this.ship.y, 'missile')
+        this.missileGroup.add(aNewMissile)
+      }
+    }
+    if (keySpaceObj.isUp === true) {
+      this.fireMissile = false
     }
   }
 }
