3e0e713f7aac8919d100a69be4ff0155786d5d02
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-23 04:08:58 +0000

    edited phaser3-12

--- js/gameScene.js
+++ js/gameScene.js
@@ -11,6 +11,7 @@
 */
   class GameScene extends Phaser.Scene {
     // create an alien
+  createAlien () {
     const alienXLocation = Math.floor(Math.random() * 1920) + 1// this will get a number between 1 and 1920
     let alienXVelocity = Math.floor(Math.random() * 50) + 1// this will get a number between 1 and 50
     alienXVelocity *= Math.round(Math.random()) ? 1 : -1// this will add minus sign in 50% of cases
@@ -18,7 +19,7 @@
     anAlien.body.velocity.y = 200
     anAlien.body.velocity.x = alienXVelocity
     this.alienGroup.add(anAlien)
-
+  }
   /** 
   * This method is the constructor 
   */
